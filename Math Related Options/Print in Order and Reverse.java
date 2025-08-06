c23062c10a172b1d3784dace1edb27c9194cd161
esai-vel-murugan <esaivelmurugans@gmail.com>
2025-08-06 20:44:03 +0530

    Create Print in Order and Reverse.java

---
+++ Math Related Options/Print in Order and Reverse.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+       int n = scan.nextInt();
+       for (int i=1;i<=n;i++){
+
+          System.out.print(i+" ");
+       }
+        for (int i=n;i>0;i--){
+
+          System.out.print(i+" ");
+       } 
+       
+    }
+}
