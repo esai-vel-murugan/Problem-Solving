5b5c8b0e267715630e0674e620a0862676b3c798
esai-vel-murugan <esaivelmurugans@gmail.com>
2025-08-06 21:01:38 +0530

    Create Print All Factors.java

---
+++ Math Related Options/Print All Factors.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        for(int i=1 ;i<=n ;i++){
+            if (n%i==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
