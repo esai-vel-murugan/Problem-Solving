d0001490fe2362e8f5cf487b4f86a42c09ba50de
esai-vel-murugan <esaivelmurugans@gmail.com>
2025-08-08 21:38:38 +0530

    Create Average of Array Elements.java

---
+++ array/Average of Array Elements.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int temp=0;
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scan.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            temp=temp+arr[i];
+        }
+        temp=temp/n;
+        System.out.println(temp);
+    }
+}
